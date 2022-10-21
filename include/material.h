80107488118f52b37d5fe92294bf00fce40f2f30
Marcel Baur <marcel@baur.gg>
2022-10-21 21:31:50 +0200

    glass

--- include/material.h
+++ include/material.h
@@ -6,43 +6,63 @@
 struct hit_record;
 
 class material {
-public:
-  virtual bool scatter(const ray &ray_in, const hit_record &rec,
-                       color &attenuation, ray &scattered) const = 0;
+  public:
+    virtual bool scatter(const ray &ray_in, const hit_record &rec,
+                         color &attenuation, ray &scattered) const = 0;
 };
 
 class lambertian : public material {
-public:
-  lambertian(const color &a) : albedo(a) {}
+  public:
+    lambertian(const color &a) : albedo(a) {}
 
-  virtual bool scatter(const ray &ray_in, const hit_record &rec,
-                       color &attenuation, ray &scattered) const override {
-    auto scatter_direction = rec.normal + random_unit_vector();
-    if (scatter_direction.near_zero())
-      scatter_direction = rec.normal;
-    scattered = ray(rec.p, scatter_direction);
-    attenuation = albedo;
-    return true;
-  }
+    virtual bool scatter(const ray &ray_in, const hit_record &rec,
+                         color &attenuation, ray &scattered) const override {
+        auto scatter_direction = rec.normal + random_unit_vector();
+        if (scatter_direction.near_zero())
+            scatter_direction = rec.normal;
+        scattered = ray(rec.p, scatter_direction);
+        attenuation = albedo;
+        return true;
+    }
 
-public:
-  color albedo;
+  public:
+    color albedo;
 };
 
 class metal : public material {
-public:
-  metal(const color &a) : albedo(a) {}
+  public:
+    metal(const color &a) : albedo(a) {}
 
-  virtual bool scatter(const ray &ray_in, const hit_record &rec,
-                       color &attenuation, ray &scattered) const override {
-    vec3 reflected = reflect(unit_vector(ray_in.direction()), rec.normal);
-    scattered = ray(rec.p, reflected);
-    attenuation = albedo;
-    return (dot(scattered.direction(), rec.normal) > 0);
-  }
+    virtual bool scatter(const ray &ray_in, const hit_record &rec,
+                         color &attenuation, ray &scattered) const override {
+        vec3 reflected = reflect(unit_vector(ray_in.direction()), rec.normal);
+        scattered = ray(rec.p, reflected);
+        attenuation = albedo;
+        return (dot(scattered.direction(), rec.normal) > 0);
+    }
 
-public:
-  color albedo;
+  public:
+    color albedo;
+};
+
+class dielectric : public material {
+  public:
+    dielectric(double index_of_refraction) : ir(index_of_refraction) {}
+
+    virtual bool scatter(const ray &r_in, const hit_record &rec,
+                         color &attenuation, ray &scattered) const override {
+        attenuation = color(1.0, 1.0, 1.0);
+        double refraction_ratio = rec.front_face ? (1.0 / ir) : ir;
+
+        vec3 unit_direction = unit_vector(r_in.direction());
+        vec3 refracted = refract(unit_direction, rec.normal, refraction_ratio);
+
+        scattered = ray(rec.p, refracted);
+        return true;
+    }
+
+  public:
+    double ir; // Index of Refraction
 };
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -9,41 +9,43 @@
 #include "../include/vec3.h"
 #include <iostream>
 
-double hit_sphere(const point3& center, double radius, const ray& r) {
+double hit_sphere(const point3 &center, double radius, const ray &r) {
     vec3 oc = r.origin() - center;
     auto a = dot(r.direction(), r.direction());
     auto half_b = dot(oc, r.direction());
-    auto c = dot(oc, oc) - radius*radius;
-    auto discriminant = half_b*half_b - a*c;
-    if (discriminant < 0) return -1.0;
-    return (-half_b - sqrt(discriminant) ) / a;
+    auto c = dot(oc, oc) - radius * radius;
+    auto discriminant = half_b * half_b - a * c;
+    if (discriminant < 0)
+        return -1.0;
+    return (-half_b - sqrt(discriminant)) / a;
 }
 
-color ray_color(const ray& r, const hittable& world, int depth) {
+color ray_color(const ray &r, const hittable &world, int depth) {
     hit_record rec;
 
-    if (depth <= 0) 
+    if (depth <= 0)
         return color(0, 0, 0);
-    if(world.hit(r, 0.001, infinity, rec)) {
+    if (world.hit(r, 0.001, infinity, rec)) {
         ray scattered;
         color attenuation;
         if (rec.mat_ptr->scatter(r, rec, attenuation, scattered))
-            return attenuation * ray_color(scattered,world, depth -1);
+            return attenuation * ray_color(scattered, world, depth - 1);
         point3 target = rec.p + rec.normal + random_in_hemisphere(rec.normal);
-        return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth-1);
+        return 0.5 * ray_color(ray(rec.p, target - rec.p), world, depth - 1);
     }
 
     vec3 unit_direction = unit_vector(r.direction());
     auto t = 0.5 * (unit_direction.y() + 1.0);
     return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
-
 }
 
-int main() {
+int main(int argc, char **argv) {
     // Image
     const auto aspect_ratio = 16.0 / 9.0;
 
-    const int image_width = 400;
+    std::string input;
+    input.append(argv[1]);
+    const int image_width = std::stoi(input);
     const int image_height = static_cast<int>(image_width / aspect_ratio);
     const int spp = 100;
     const int max_depth = 50;
@@ -51,14 +53,16 @@ int main() {
     // World
     hittable_list world;
     auto material_ground = make_shared<lambertian>(color(0.8, 0.8, 0.0));
-    auto material_center = make_shared<lambertian>(color(0.7, 0.3, 0.3));
-    auto material_left   = make_shared<metal>(color(0.8, 0.8, 0.8));
-    auto material_right  = make_shared<metal>(color(0.8, 0.6, 0.2));
+    auto material_center = make_shared<dielectric>(1.5);
+    auto material_left = make_shared<dielectric>(1.5);
+    auto material_right = make_shared<metal>(color(0.8, 0.6, 0.2));
 
-    world.add(make_shared<sphere>(point3( 0.0, -100.5, -1.0), 100.0, material_ground));
-    world.add(make_shared<sphere>(point3( 0.0,    0.0, -1.0),   0.5, material_center));
-    world.add(make_shared<sphere>(point3(-1.0,    0.0, -1.0),   0.5, material_left));
-    world.add(make_shared<sphere>(point3( 1.0,    0.0, -1.0),   0.5, material_right));
+    world.add(
+        make_shared<sphere>(point3(0.0, -100.5, -1.0), 100.0, material_ground));
+    world.add(
+        make_shared<sphere>(point3(0.0, 0.0, -1.0), 0.5, material_center));
+    world.add(make_shared<sphere>(point3(-1.0, 0.0, -1.0), 0.5, material_left));
+    world.add(make_shared<sphere>(point3(1.0, 0.0, -1.0), 0.5, material_right));
 
     // Camera
     camera cam;
@@ -66,13 +70,13 @@ int main() {
     // Render
     std::cout << "P3\n" << image_width << ' ' << image_height << "\n255\n";
 
-    for (int j = image_height-1; j >= 0; --j) {
+    for (int j = image_height - 1; j >= 0; --j) {
         std::cerr << "\rScanlines remaining: " << j << std::flush;
         for (int i = 0; i < image_width; ++i) {
             color pixel_color(0, 0, 0);
             for (int s = 0; s < spp; ++s) {
-                auto u = (i + random_double()) / (image_width-1);
-                auto v = (j + random_double()) / (image_height-1);
+                auto u = (i + random_double()) / (image_width - 1);
+                auto v = (j + random_double()) / (image_height - 1);
                 ray r = cam.get_ray(u, v);
                 pixel_color += ray_color(r, world, max_depth);
             }
@@ -82,4 +86,3 @@ int main() {
 
     std::cerr << "\nDone.\n";
 }
-
